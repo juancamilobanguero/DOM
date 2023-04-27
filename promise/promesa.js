e2f174c8a260941489ca4e07d6eedaa50b97dd19
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-04-27 18:43:42 -0500

    camilo2334

---
+++ promise/promesa.js
@@ -0,0 +1,70 @@
+// class Persona {
+
+//     //constructor
+//     // constructor(nombre = "", apellido = ""){
+//     //     this.nombre = nombre
+//     //     this.apellido = apellido
+//     // }
+
+//     //atributos
+//     nombre
+//     apellido
+//     // edad
+//     // genero
+//     // altura
+//     // peso
+//     // estado_civil
+//     // etnia
+//     // n_doc
+
+//     //metodos
+//     hablar(){
+//         console.log(soy ${this.nombre});
+//     }
+
+//     setNombre(nombre = ""){
+//         this.nombre = nombre
+//     }
+
+//     getNombre(){
+//        return this.nombre 
+//     }
+// }
+
+// class Alumno extends Persona {
+ 
+//     hablar(){
+
+//     }
+
+// }
+
+// const user = new Persona()
+// user.setNombre("Carlos")
+// user.hablar()
+// let user2 = new Persona("Miguel", "PHP")
+// user2.hablar()
+
+// let x = () => {
+//     console.log("Soy una x publica")
+// }
+//x()
+// let p = new Promise((resolve, reject) => {
+
+//     let x = false
+
+//     if(x){
+//         resolve("Promesa exitosa")
+//     } else {
+//         reject("Promesa fallida")
+//     }
+
+// })
+// console.log(p)
+
+// p.then((data) => {
+//     console.log(data)
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
